9d68abb4a862d476b7f04d2b91f1ec068caacbb8
HurayraIIT <hurayraiit@gmail.com>
2020-01-29 20:39:48 +0600

    added Java folder for java practice

---
+++ Java/Test.java
@@ -0,0 +1,13 @@
+package Java;
+public class Test
+{
+
+    public static void main(final String[] args)
+    {
+        
+        final String x = "Abu Hurayra yes" ;
+        System.out.println("Hello World!! && " + x);
+
+    }
+
+}
