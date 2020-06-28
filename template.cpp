4732bef37f2f1e1a79a6330c9f28cf6f31901fa6
HurayraIIT <hurayraiit@gmail.com>
2020-06-28 18:10:53 +0600

    added template

--- template.cpp
+++ template.cpp
@@ -41,34 +41,9 @@ int main()
 {
     fast_io
     
-	int t;
-	cin >> t ;
-	while(t--)
-	{
-		string s ;
-		cin >> s ;
-		LL ans = 0 ;
-		LL flg = -1 ;
-		LL n = s.size();
-		LL a[n];
-		for(int i = 0 ; i < n ; i++ )
-		{
-			if(!i) a[i] = (s[i]=='-') ? -1 : 1 ;
-			else a[i] = a[i-1] + ( (s[i]=='-') ? -1 : 1 ) ;
-		}
-		for(int i = 0 ; i < n ; i++ )
-		{
-			if(a[i]==flg)
-			{
-				ans += i+1 ; flg--;
-			}
-		}
-		ans += n ;
-		cout << ans << endl;
-	}
-	
-	
-	return 0;
+    
+    
+    return 0;
 }
 
 
