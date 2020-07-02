75a6419230d52834412e2d7216339a9927199af9
HurayraIIT <hurayraiit@gmail.com>
2020-07-02 14:55:08 +0600

    random practice

---
+++ codeforces/random/A_Phoenix_and_Balance.cpp
@@ -0,0 +1,65 @@
+// Abu Hurayra (HurayraIIT)
+// IIT, Jahangirnagar University
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+using namespace std;
+
+typedef long long LL;
+typedef long long int int64;
+typedef unsigned long long int uint64;
+typedef vector<int> vi;
+
+#define READ freopen("input.txt", "r", stdin);
+#define WRITE freopen("output.txt", "w", stdout);
+#define fast_io                   \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);
+#define sz(a) int((a).size())
+#define space " "
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+#define pi acos(-1.0)
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define MOD 1000000007
+#define PRECISION(x) cout << fixed << setprecision(x);
+
+int main()
+{
+    fast_io int t, n, i;
+    cin >> t;
+    while (t--)
+    {
+        cin >> n;
+        int ans = 0 , a = 0 , b = 0 ;
+        for(i=1;i<n/2;i++)
+        {
+            a += (1<<i) ;
+        }
+        a += (1<<n) ;
+        for(i=n/2;i<n;i++)
+        {
+            b += (1<<i) ;
+        }
+        ans = abs(a-b) ;
+        cout << ans << endl;
+    }
+
+    return 0;
+}
+// 2 4 8 16 32 64
+// 2 4 8 16 32 64 128 256
