950ccc30c9885f2f9d7ccd26c0c18e936af3a5ae
HurayraIIT <hurayraiit@gmail.com>
2020-07-02 14:54:24 +0600

    random practice

---
+++ codeforces/random/A_Candies_and_Two_Sisters.cpp
@@ -0,0 +1,61 @@
+// Abu Hurayra (HurayraIIT)
+// IIT, Jahangirnagar University
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+using namespace std;
+
+typedef long long LL;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+typedef vector<int> vi; 
+
+#define READ freopen("input.txt", "r", stdin);
+#define WRITE freopen("output.txt", "w", stdout);
+#define fast_io ios_base::sync_with_stdio(0);  cin.tie(0);
+#define sz(a) int((a).size())
+#define space " "
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+#define pi acos(-1.0)
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define MOD 1000000007
+#define PRECISION(x) cout << fixed << setprecision(x); 
+ 
+ 
+int main()
+{
+    fast_io
+    LL t , n ;
+    cin >> t ;
+    while(t--)
+    {
+        cin >> n ;
+        if(n<=2) cout << 0 << endl;
+        else
+        {
+            cout << n - (n/2) - 1 << endl;
+        }
+        
+    }
+    
+    
+    return 0;
+}
+
+
+ 
