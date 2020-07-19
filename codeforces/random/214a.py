beea87fc87f700c661499fac694a63cf257df71e
HurayraIIT <hurayraiit@gmail.com>
2020-07-19 12:29:19 +0600

    added a new python file

---
+++ codeforces/random/214a.py
@@ -0,0 +1,26 @@
+import sys
+def rs(): return sys.stdin.readline().rstrip()
+def ri(): return int(sys.stdin.readline())
+def ria(): return list(map(int, sys.stdin.readline().split()))
+def ws(s): sys.stdout.write(s + '\n')
+def wi(n): sys.stdout.write(str(n) + '\n')
+def wia(a): sys.stdout.write(' '.join([str(x) for x in a]) + '\n')
+#a = list(map(int, input().split()))
+ 
+ 
+ 
+def main():
+    n, m = ria()
+    count = 0
+    for i in range(0,1001):
+        for j in range(0,1001):
+            if i*i+j==n and i+j*j==m:
+                count += 1
+    wi(count)
+    
+
+
+ 
+ 
+if __name__ == '__main__':
+    main()
