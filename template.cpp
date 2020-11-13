e5bc0dfd3d66b3407a6d562687192da671aa9580
Abu Hurayra <hurayraiit@gmail.com>
2020-11-13 15:07:05 +0600

    directory junk Cleanup

--- template.cpp
+++ template.cpp
@@ -37,8 +37,8 @@ typedef vector<pii> vpii;
 #define INF (int)1e9
 #define MOD 1000000007
 #define PRECISION(x) cout << fixed << setprecision(x);
-#define rep(i,n) for(int ( i ) = ( 0 ) ;( i ) <( n );( i )++)
-#define rep2(i,a,b) for(int ( i ) = ( a ) ;( i ) <= ( b );( i )++)
+#define rep(i,n) for(int i = 0 ; i < n ; i++)
+#define rep2(i,a,b) for(int i = a ; i <= b ; i++)
  
  
 int main()
