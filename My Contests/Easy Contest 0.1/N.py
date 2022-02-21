63c177590d40190537cc9648851efc8a86208fb5
Abu Hurayra <hurayraiit@gmail.com>
2022-02-21 11:54:41 +0600

    Solved L, M, N

---
+++ My Contests/Easy Contest 0.1/N.py
@@ -0,0 +1,30 @@
+# Bismillahir Rahmanir Rahim
+# Abu Hurayra - Handle: HurayraIIT
+import sys
+import math
+def mp(): return map(int, sys.stdin.readline().split())
+def rs(): return sys.stdin.readline().rstrip()
+def ri(): return int(sys.stdin.readline())
+def ria(): return list(map(int, sys.stdin.readline().split()))
+def ws(s): sys.stdout.write(s + '\n')
+def wi(n): sys.stdout.write(str(n) + '\n')
+def wia(a): sys.stdout.write(' '.join([str(x) for x in a]) + '\n')
+#a = list(map(int, input().split()))
+ 
+ 
+ 
+def main(tc:int):
+    n = ri()
+    
+    p = int(math.sqrt(n)) 
+
+    while p * (p+1) < 2 * n:
+        p += 1
+    
+    wi(p)
+ 
+ 
+if __name__ == '__main__':
+    t = 1
+    for tc in range(1,t+1):
+        main(tc)
