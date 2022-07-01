b4ae8f247143f47eceb6c382ee5bea2c17893f59
Abu Hurayra <hurayraiit@gmail.com>
2022-07-01 16:19:02 +0600

    updated the template

--- python_template.py
+++ python_template.py
@@ -1,5 +1,10 @@
 # Abu Hurayra
 import sys
+from collections import defaultdict
+import threading
+threading.stack_size(2**27)
+sys.setrecursionlimit(2**21)
+
 def rs(): return sys.stdin.readline().rstrip()
 def ri(): return int(sys.stdin.readline())
 def ria(): return list(map(int, sys.stdin.readline().split()))
@@ -10,8 +15,10 @@ def wia(a): sys.stdout.write(' '.join([str(x) for x in a]) + '\n')
 
 
 def main():
-    pass
+    print("hello")
 
 
 if __name__ == '__main__':
-    main()
+    t = threading.Thread(target=main)
+    t.start()
+    t.join()
